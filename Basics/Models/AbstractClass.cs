e5dcac2ee10c05505b0e0bc1339babd9f121dce7
Jonas <jonas.zeindl@gmail.com>
2026-09-16 12:46:10 +0200

    Projektdateien hinzufügen.

---
+++ Basics/Models/AbstractClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics.Models
+{
+    // abstrakte Klasse: 
+    //              eine abstrakte Klasse beinhaltet alles, was eine normale
+    //              Klasse auch beinhaltet (fields, properties, ctors, methods, ...),
+    //              zusätzlich beinhaltet sie abstrakte Methoden
+    // abstrakte Methode: besitzt nur einen Methodenkopf, aber keinen Methodencode
+
+
+    public abstract class AbstractClass
+    {
+
+        private int _value;
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                if(value >= 0)
+                {
+                    _value = value;
+                }
+            }
+        }
+
+        protected AbstractClass()
+        {
+            Value = 0;
+        }
+
+        public string Do()
+        {
+            return "abstrakt";
+        }
+
+        // abstrakte Methode
+        //      besitzt keinen Code
+        public abstract double Calculate();
+
+    }
+}
---
+++ Basics/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
